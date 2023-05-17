5ba6c673aa40f6f8a77946f515275dc77426ceba
Asher <ashmenzer@gmail.com>
2023-05-17 07:01:49 +0300

    update the main

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 // Chess 
 #include "Chess.h"
+#include "Board.h"
 
+//int generateCodeResponse(int src_row, int src_col, int dst_row, int dst_col, Board* game_board);
 int main()
 {
 	string board = "RNBQKBNRPPPPPPPP################################pppppppprnbqkbnr"; 
 //	string board = "##########K###############################R#############r#r#####";
 	Chess a(board);
+	Board gameBoard(board);
 	int codeResponse = 0;
 	string res = a.getInput();
 	while (res != "exit")
@@ -24,13 +27,20 @@ int main()
 		42 - the last movement was legal, next turn 
 		*/
 
-		/**/ 
-		{ // put your code here instead that code
+		/* 
+		{
+			
+			
+			// put your code here instead that code
 			cout << "code response >> ";
 			cin >> codeResponse;
 		}
-		/**/
-
+		*/
+		int src_row = res[0] - 'a';
+		int src_col = res[1] - '0' - 1;
+		int dst_row = res[2] - 'a';
+		int dst_col = res[3] - '0' - 1;
+		codeResponse = gameBoard.do_move(Location(src_row,src_col), Location(dst_row, dst_col));
 		a.setCodeResponse(codeResponse);
 		res = a.getInput(); 
 	}
@@ -38,3 +48,14 @@ int main()
 	cout << endl << "Exiting " << endl; 
 	return 0;
 }
+
+
+
+//int generateCodeResponse(int src_row, int src_col, int dst_row, int dst_col, Board* game_board) {
+//	Piece* src_piece = game_board->getPiece(src_row, src_col);
+//	if (src_piece == 0) {
+//		return 11;
+//	}
+//
+//
+//}
