e8f4dbee8e89bd452eb911d369cb0f6fd8d11c58
Asher <ashmenzer@gmail.com>
2023-05-17 05:38:24 +0300

    add doc

--- include/Board.h
+++ include/Board.h
@@ -15,14 +15,46 @@ public:
 	Board();
 	Board(std::string pieces);
 	~Board();
+	/**
+	 * @param The location row.
+	 * @param The location column.
+	 * @return The piece in this location, nullptr if no piece there.
+	 */
 	Piece* getPiece(int row, int col);
+	/**
+	 * @return True if is the white turn, false is the black turn.
+	 */
 	bool getPlayerColor();
+	/**
+	* @Brief Move the piece from source location to the destanation location
+	 * @param The location source.
+	 * @param The location destanation.
+	 */
 	void movePiece(Location src_location, Location _location);
+	/**
+	 * @Brief Undo move of the piece from source location to the destanation location.
+	 * @param The location source.
+	 * @param The location destanation.
+	 * @param The piece to move.
+	 */
 	void undoMove(Location src_location, Location dst_location,Piece* piece);
+	/**
+	* @param The player to check true=white ,false=black.
+	 * @return True if there is chekc on this player, false is not.
+	 */
 	bool isInCheck(bool player);
+	/**
+	* @param The player to check true=white ,false=black.
+	 * @return True if there is chekc mate on this player, false is not.
+	 */
 	bool isCheckMate(bool player);
-	int do_move(Location src_location, Location dst_location);
-	//Piece** getGameBoarg();
+	/**
+	* @Brief Do command of Move the piece from source location to the destanation location
+	 * @param The location source.
+	 * @param The location destanation.
+	 * @return The code response to this command.
+	 */
+	int do_command(Location src_location, Location dst_location);
 private:
 	Piece* _gameBoard[ROW_SIZE][COL_SIZE];
 	Piece* _white_king;
